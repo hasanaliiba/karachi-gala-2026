3540f6a72ba71f9f1f59d11e6565618afc1a93dc
Hasan Ali <Hasanaliiba@gmail.com>
2026-04-24 18:06:45 +0500

    feat: add AdminSeeder with Iqra admin and early_bird_date setting

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::firstOrCreate(
+            ['email' => 'iqra@kgl.com'],
+            [
+                'name'     => 'Iqra',
+                'password' => Hash::make('kgl123'),
+            ]
+        );
+
+        Setting::firstOrCreate(
+            ['key' => 'early_bird_date'],
+            ['value' => '2026-05-28']
+        );
+    }
+}
