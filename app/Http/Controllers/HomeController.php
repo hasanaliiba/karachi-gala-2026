595156145f95fa7c1f390e35b74027b3634b2628
Hasan Ali <Hasanaliiba@gmail.com>
2026-04-27 19:58:55 +0500

    feat: pass galleryItems from HomeController to welcome page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GalleryItem;
 use App\Models\Setting;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 use Laravel\Fortify\Features;
@@ -14,6 +16,15 @@ class HomeController extends Controller
         return Inertia::render('welcome', [
             'canRegister'   => Features::enabled(Features::registration()),
             'earlyBirdDate' => Setting::get('early_bird_date', '2026-05-28'),
+            'galleryItems'  => GalleryItem::orderBy('sort_order')
+                ->get()
+                ->map(fn (GalleryItem $item) => [
+                    'id'         => $item->id,
+                    'label'      => $item->label,
+                    'image_url'  => Storage::disk('public')->url($item->image_path),
+                    'wide'       => $item->wide,
+                    'sort_order' => $item->sort_order,
+                ]),
         ]);
     }
 }
